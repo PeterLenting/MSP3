9fffe12fdad8377af59ce1f975e2b7db9d0c8436
PeterLenting <pplenting@gmail.com>
2019-10-03 13:19:18 +0000

    Update met CI-code

--- app.py
+++ app.py
@@ -1,23 +1,15 @@
-import pymongo
 import os
+from flask import Flask
 
-MONGODB_URI = os.getenv("MONGO_URI")
-DBS_NAME = "myTestDB"
-COLLECTION_NAME = "myFirstMDB"
+app = Flask(__name__)
 
-def mongo_connect(url):
-    try:
-        conn = pymongo.MongoClient(url)
-        print("Mongo is connected!")
-        return conn
-    except pymongo.errors.ConnectionFailure as e:
-        print("Could not connect to MongoDB: %s") % e
 
-conn = mongo_connect(MONGODB_URI)
+@app.route('/')
+def hello():
+    return 'Hello World ...again'
 
-coll = conn[DBS_NAME][COLLECTION_NAME]
 
-documents = coll.find()
-
-for doc in documents:
-    print(doc)
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
